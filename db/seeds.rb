e46ad5208208bc7b9a8fcbbdc726c24b10afcdb9
Philippe Coste <philippecostelh@gmail.com>
2017-05-18 14:03:41 +0200

    Controllers created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: 'Mojito')
+Cocktail.create(name: 'Old Moscow')
+Cocktail.create(name: 'Blue Paris')
+Cocktail.create(name: 'Sex on the Beach')
+
+Ingredient.create(name: 'citron')
+Ingredient.create(name: 'sucre')
+Ingredient.create(name: 'vodka')
+Ingredient.create(name: 'jus de Papaye')
+Ingredient.create(name: 'whisky')
+
+
+dose1 = Dose.new(description: '2 ml', cocktail_id: '1', ingredient_id: '1')
+dose1.save
+dose2 = Dose.new(description: '3 ml', cocktail_id: '2', ingredient_id: '3')
+dose2.save
+dose3 = Dose.new(description: '4 ml', cocktail_id: '4', ingredient_id: '4')
+dose3.save
+dose4 = Dose.new(description: '5 ml', cocktail_id: '3', ingredient_id: '5')
+dose4.save
+dose5 = Dose.new(description: '6 ml', cocktail_id: '2', ingredient_id: '1')
+dose5.save
